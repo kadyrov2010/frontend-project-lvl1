3c15f996b1f55558e1017d755aa7e7ff890829a3
Aleks_Kad <kadyrov2010@gmail.com>
2019-10-08 23:08:58 +0300

    add Brain-Calc

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export const randomNumb = (min = 0, max = 189) => Math.floor(Math.random() * (max - min + 1)) + min;
-export const firstGame = () => {
+export const brainEven = () => {
   console.log('Welcome to the Brain Games!\nAnswer "yes" if the number is even, otherwise answer "no".\n');
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}\n`);
@@ -19,6 +19,52 @@ export const firstGame = () => {
       break;
     }
     count += 1;
+  }
+  if (count === 3) {
+    console.log(`Congratulations, ${username}!`);
+  } else {
+    console.log(`Let's try again, ${username}!`);
+  }
+};
+
+export const brainCalc = () => {
+  console.log('Welcome to the Brain Games!\nWhat is the result of the expression?\n');
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}\n`);
+  let count = 0;
+  while (count < 3) {
+    count += 1;
+    const cos = randomNumb(1, 3);
+    const num1 = randomNumb(1, 10);
+    const num2 = randomNumb(1, 10);
+    if (cos === 1) {
+      console.log(`Question: ${num1} + ${num2}`);
+      const answer = readlineSync.question('Your aswer: ');
+      if (num1 + num2 === Number(answer)) {
+        console.log('Correct!');
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
+        break;
+      }
+    } else if (cos === 2) {
+      console.log(`Question: ${num1} - ${num2}`);
+      const answer = readlineSync.question('Your aswer: ');
+      if (num1 - num2 === Number(answer)) {
+        console.log('Correct!');
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
+        break;
+      }
+    } else if (cos === 3) {
+      console.log(`Question: ${num1} * ${num2}`);
+      const answer = readlineSync.question('Your aswer: ');
+      if (num1 * num2 === Number(answer)) {
+        console.log('Correct!');
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
+        break;
+      }
+    }
   }
   if (count === 3) {
     console.log(`Congratulations, ${username}!`);
