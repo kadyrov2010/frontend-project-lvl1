3d658a6db9c8ed87b75b74b96e75f056765171c1
Aleks_Kad <kadyrov2010@gmail.com>
2019-12-03 22:13:05 +0200

    refactoring

--- src/games/calc.js
+++ src/games/calc.js
@@ -9,23 +9,22 @@ const minNum = 0;
 const maxNum = 15;
 const subscript = 0;
 const superscript = 2;
-const operandChoice = operand[randomNumb(subscript, superscript)];
-const firstNum = randomNumb(minNum, maxNum);
-const secondNum = randomNumb(minNum, maxNum);
-
-const getOperand = () => {
-  switch (operandChoice) {
-    case '+': return String(firstNum + secondNum);
-    case '*': return String(firstNum * secondNum);
-    default: return String(firstNum - secondNum);
-  }
-};
 
 const generateCondition = () => {
-  const correctAnswer = getOperand();
+  const firstNum = randomNumb(minNum, maxNum);
+  const secondNum = randomNumb(minNum, maxNum);
+  const operandChoice = operand[randomNumb(subscript, superscript)];
+  const getOperand = (choice) => {
+    switch (choice) {
+      case '+': return String(firstNum + secondNum);
+      case '*': return String(firstNum * secondNum);
+      default: return String(firstNum - secondNum);
+    }
+  };
+  const correctAnswer = getOperand(operandChoice);
   const question = `${firstNum} ${operandChoice} ${secondNum}`;
-  return cons(question, correctAnswer());
+  return cons(question, correctAnswer);
 };
-const gameCalc = () => playGame(ruleGame, generateCondition);
+const gameCalc = () => playGame(generateCondition, ruleGame);
 
 export default gameCalc;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,32 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
 
-const playRounds = (conditionsGame, currentRound, roundsToWin) => {
-  const regulat = conditionsGame();
-  const quest = car(regulat);
-  const correctAnswer = cdr(regulat);
-
-  if (currentRound > roundsToWin) {
-    return true;
-  }
-
-  console.log(`Question: ${quest}`);
-  const answer = readlineSync.question('Your answer: ');
-
-  if (answer === correctAnswer) {
-    console.log('correct!');
-  } else {
-    console.log(`'${answer}' is wrong answer ;( Correct answer was '${correctAnswer}'`);
-    return false;
-  }
-  return playRounds(conditionsGame, currentRound + 1, roundsToWin);
-};
-
 const roundsToWin = 3;
-const currentRound = 1;
 
-const playGame = (ruleGame, conditionsGame) => {
+const playGame = (dataGame, rule) => {
   console.log('Welcome to the Brain Games!\n');
-  console.log(`${ruleGame}\n`);
+  console.log(`${rule}\n`);
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}\n`);
-  const isWinner = playRounds(conditionsGame, currentRound, roundsToWin);
+  const playRounds = (data, rounds) => {
+    const regulat = data();
+    const question = car(regulat);
+    const correctAnswer = cdr(regulat);
+    if (rounds === 0) {
+      return true;
+    }
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (correctAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;( Correct answer was '${correctAnswer}'`);
+      return false;
+    }
+    return playRounds(dataGame, rounds - 1);
+  };
+  const isWinner = playRounds(dataGame, roundsToWin);
   if (isWinner) {
     console.log(`Congratulations, ${name}!`);
   } else {
