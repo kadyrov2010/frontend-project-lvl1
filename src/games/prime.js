9b2a3423e415e03442e11e50ddb77620ab9d3b3b
Alexandr <48246969+kadyrov2010@users.noreply.github.com>
2020-02-12 12:50:09 +0200

    fix bug pime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -8,7 +8,7 @@ const isPrime = (x) => {
   if (x < 2) {
     return false;
   }
-  for (let i = 2; i < (x / 2); i += 1) {
+  for (let i = 2; i <= (x / 2); i += 1) {
     if (x % i === 0) {
       return false;
     }
